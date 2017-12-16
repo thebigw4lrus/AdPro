1c1b063ed0d2e4d11a12fa7514ae397915d1dcaa
Javier Contreras <jcontreras000@gmail.com>
2017-12-16 20:40:08 +0100

    Fix rubocop violation introduced by rails generator

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
 source 'https://rubygems.org'
 
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'mysql2', '0.3.18'
 gem 'rails', '4.0.0'
 gem 'sass-rails', '~> 4.0.0'
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
-gem 'jquery-rails'
 gem 'turbolinks'
-gem 'jbuilder', '~> 1.2'
-gem 'mysql2', '0.3.18'
+gem 'uglifier', '>= 1.3.0'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,4 @@
 AdPro::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -14,7 +12,7 @@ AdPro::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
-  config.static_cache_control = "public, max-age=3600"
+  config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true

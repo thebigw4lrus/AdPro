51f43b65e7e0d52bf0944ca6baa592385eb175fc
Javier Contreras <jcontreras000@gmail.com>
2017-12-16 17:32:25 +0100

    App Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdPro::Application.config.secret_key_base = 'eabd32cef08172f70e89b121d845cac8580c61a7e1f6adcbdbf68b9d850e1f51b52498f618fca5d219b27b99367049adb2a5f786a2fb38070ed42676ac8bb71d'

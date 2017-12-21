af856c390ca95d3ee3cbc42acbe6adf5678aab7f
Javier Contreras <jcontreras000@gmail.com>
2017-12-21 18:25:28 +0100

    Integration tests added to Ads endpoint

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 RSpec.describe AdPro::V1 do
   describe 'Integration' do
-    before do
-      Timecop.freeze(Date.new(2015, 10, 21))
+    let(:banners) do
+      get('banners/')
+      JSON.parse(response.body).map do |banner|
+        [banner['name'], banner['id']]
+      end.to_h
     end
 
-    after { Timecop.return }
+    let(:campaigns) do
+      get('campaigns/')
+      JSON.parse(response.body).map do |campaign|
+        [campaign['name'], campaign['id']]
+      end.to_h
+    end
 
-    it 'manages properly all operations campaign/banners related' do
+    before do
+      Timecop.freeze(Date.new(2015, 10, 21))
+
       # Creates all banners
       banner1 = { name: 'banner1', url: 'http://somebanner1' }
       banner2 = { name: 'banner2', url: 'http://somebanner2' }
@@ -21,18 +31,11 @@ RSpec.describe AdPro::V1 do
       parameters = { name: 'campaign1' }
       post('campaigns/',
            parameters.to_json, 'CONTENT_TYPE' => 'application/json')
+    end
 
-      # Gets banners and campaigns ids
-      get('banners/')
-      banners = JSON.parse(response.body).map do |banner|
-        [banner['name'], banner['id']]
-      end.to_h
+    after { Timecop.return }
 
-      get('campaigns/')
-      campaigns = JSON.parse(response.body).map do |campaign|
-        [campaign['name'], campaign['id']]
-      end.to_h
-
+    it 'manages properly all operations campaign/banners related' do
       campaign_id = campaigns['campaign1']
       banner1_id = banners['banner1']
       banner2_id = banners['banner2']
@@ -171,6 +174,52 @@ RSpec.describe AdPro::V1 do
             'name' => 'banner2',
             'url' => 'http://somebanner2',
             'time_slot' => 13
+          }
+        ]
+      }
+      expect(json).to eq(expected)
+    end
+
+    it 'retrieves all possible ads at 14:00' do
+      campaign_id = campaigns['campaign1']
+      banner1_id = banners['banner1']
+      banner2_id = banners['banner2']
+
+      # Set up external IpToTime service to
+      # retrieve 14:00
+      ip_to_time = double('ip_to_time')
+      allow(ip_to_time).to receive(:get)
+        .and_return(DateTime.new(2015, 1, 1, 14, 0, 0))
+      allow(AdPro::IpToTime).to receive(:new).and_return(ip_to_time)
+
+      # A campaign is configured with
+      # banners at 14 and 17
+      input = {
+        'id' => campaigns['campaign1'],
+        'banners' => [
+          {
+            'banner_id' => banner1_id,
+            'time_slot' => 14
+          },
+          {
+            'banner_id' => banner2_id,
+            'time_slot' => 17
+          }
+        ]
+      }
+      put("campaigns/#{campaign_id}/banners",
+          input.to_json, 'CONTENT_TYPE' => 'application/json')
+
+      # Get all ads (It will  try to get the one at 14:00)
+      get('ads/')
+      json = JSON.parse(response.body)
+
+      expected = {
+        'banners' => [
+          {
+            'id' => banner1_id,
+            'name' => 'banner1',
+            'url' => 'http://somebanner1'
           }
         ]
       }
